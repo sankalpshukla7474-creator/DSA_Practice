893c449e981c4f752aad8910796229cdc4034bde
Sankalp Shukla <sankalpshukla7474@gmail.com>
2026-04-19 10:13:57 +0530

    Time: 2 ms (94.87%) | Memory: 82.2 MB (34.60%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -16,13 +16,30 @@
 class Solution {
     public int minDepth(TreeNode root) {
         if(root == null) return 0 ;
-        if(root.left == null && root.right == null) return 1 ;
-        if(root.left == null){
-            return 1 + minDepth(root.right) ;
+        Queue<Pair> q = new LinkedList<>() ;
+        q.offer(new Pair(root,1)) ;
+        while(!q.isEmpty()){
+            Pair p = q.poll() ;
+            TreeNode roo = p.node ;
+            int k = p.val ;
+            if(roo.left == null && roo.right == null){
+                return k ;
+            }
+            if(roo.left != null){
+                q.offer(new Pair(roo.left,k+1)) ;
+            }
+            if(roo.right != null){
+                q.offer(new Pair(roo.right,k+1)) ;
+            }
         }
-        if(root.right == null){
-            return 1 + minDepth(root.left) ;
+        return -1 ;
+    }
+    class Pair{
+        TreeNode node ;
+        int val ;
+        Pair(TreeNode node , int val){
+            this.node = node ;
+            this.val = val ;
         }
-        return 1 + Math.min(minDepth(root.right) , minDepth(root.left) ) ;
     }
 }

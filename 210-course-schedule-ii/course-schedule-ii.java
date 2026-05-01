4184fe69e455367e23bebd020cde3ef2e93d7123
Sankalp Shukla <sankalpshukla7474@gmail.com>
2026-05-01 10:57:00 +0530

    Time: 7 ms (35.02%) | Memory: 47.9 MB (20.35%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,53 @@
+class Solution {
+    List<Integer> ls = new ArrayList<>() ;
+    List<List<Integer>> adj = new ArrayList<>() ;
+    public int[] findOrder(int num, int[][] p) {
+        for(int i = 0 ; i < num ; i++){
+            adj.add(new ArrayList<>()) ;
+        }
+        int[] deg = new int[num] ;
+        for(int i = 0 ; i < p.length ; i++){
+            int a = p[i][0] ;
+            int b = p[i][1] ;
+            adj.get(b).add(a) ;
+            deg[a]++ ;
+        }
+        boolean[] visit = new boolean[num] ;
+        Queue<Integer> q = new LinkedList<>() ;
+        List<Integer> pb = new ArrayList<>() ;
+        for(int i = 0 ; i < num ; i++){
+            if(deg[i] == 0){
+                q.offer(i) ;
+            }
+        }
+        while(!q.isEmpty()){
+            int u = q.poll() ;
+            pb.add(u) ;
+            for(int v : adj.get(u)){
+                deg[v]-- ;
+                if(deg[v] == 0) q.offer(v) ;
+            }
+        }
+        if(pb.size() != num) return new int[0] ;
+        int[] arr = new int[num] ;
+        for(int i = 0 ; i < num ; i++){
+            if(!visit[i]){
+                dfs(i,visit) ;
+            }
+        }
+        Collections.reverse(ls) ;
+        for(int i = 0 ; i < num ; i++){
+            arr[i] = ls.get(i) ;
+        }
+        return arr ;
+    }
+    public void dfs(int u , boolean[] visit){
+        visit[u] = true ;
+        for(int v : adj.get(u)){
+            if(!visit[v]){
+                dfs(v,visit) ;
+            }
+        }
+        ls.add(u) ;
+    }
+}

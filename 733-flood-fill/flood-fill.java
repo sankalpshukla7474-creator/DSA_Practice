f28938e26db1b5a62492e529b606ca3761ddbb66
Sankalp Shukla <sankalpshukla7474@gmail.com>
2026-05-05 10:21:08 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (61.31%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int n = image.length ;
+        int m = image[0].length ;
+        boolean[][] visit = new boolean[n][m] ;
         int prev = image[sr][sc] ;
-        if(prev == color) return image;
-        boolean[][] visit = new boolean[image.length][image[0].length] ;
-        dfs(image,sr,sc,color,prev,visit) ;
+        dfs(image,sr,sc,n,m,color,visit,prev) ;
         return image ;
     }
-    public void dfs(int[][] image , int i , int j , int color , int prev , boolean[][] visit ){
-         if(i < 0 || i >= image.length || j < 0 || j >= image[0].length) return;
-
-        if(visit[i][j] || image[i][j] != prev ) return ;
-        image[i][j] = color ;
+    public void dfs(int[][] ima , int i , int j , int n , int m , int color , boolean[][] visit, int prev){
+        if(i < 0 || i >= n || j < 0 || j >= m ) return ;
+        if(visit[i][j] || ima[i][j] != prev) return ;
+        ima[i][j] = color ;
         visit[i][j] = true ;
-        dfs(image,i+1,j,color,prev,visit) ;
-        dfs(image,i,j+1,color,prev,visit) ;
-        dfs(image,i-1,j,color,prev,visit) ;
-        dfs(image,i,j-1,color,prev,visit) ;
+        dfs(ima,i+1,j,n,m,color,visit,prev) ;
+        dfs(ima,i,j+1,n,m,color,visit,prev) ;
+        dfs(ima,i-1,j,n,m,color,visit,prev) ;
+        dfs(ima,i,j-1,n,m,color,visit,prev) ;
     }
 }

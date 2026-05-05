9bf2b2ace3a5afca48c8b8c20d1e9262464c7af7
Sankalp Shukla <sankalpshukla7474@gmail.com>
2026-05-05 10:27:41 +0530

    Time: 6 ms (48.93%) | Memory: 46.9 MB (56.58%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,34 +1,40 @@
 class Solution {
-    List<Integer> ls = new ArrayList<>() ;
-    List<List<Integer>> adj = new ArrayList<>() ;
-    public int[] findOrder(int num, int[][] p) {
+    public int[] findOrder(int num, int[][] pre) {
+        List<List<Integer>> adj = new ArrayList<>() ;
         for(int i = 0 ; i < num ; i++){
             adj.add(new ArrayList<>()) ;
         }
-        int[] deg = new int[num] ;
-        for(int i = 0 ; i < p.length ; i++){
-            int a = p[i][0] ;
-            int b = p[i][1] ;
-            adj.get(b).add(a) ;
-            deg[a]++ ;
+        int[] indeg = new int[num] ;
+        for(int i = 0 ; i < pre.length ; i++){
+            int u = pre[i][0] ;
+            int b = pre[i][1] ;
+            adj.get(b).add(u) ;
+            indeg[u]++ ;
         }
-        boolean[] visit = new boolean[num] ;
         Queue<Integer> q = new LinkedList<>() ;
-        List<Integer> pb = new ArrayList<>() ;
         for(int i = 0 ; i < num ; i++){
-            if(deg[i] == 0){
+            if(indeg[i] == 0){
                 q.offer(i) ;
             }
         }
+        List<Integer> ls = new ArrayList<>() ;
         while(!q.isEmpty()){
             int u = q.poll() ;
-            pb.add(u) ;
+            ls.add(u) ;
             for(int v : adj.get(u)){
-                deg[v]-- ;
-                if(deg[v] == 0) q.offer(v) ;
+                indeg[v]-- ;
+                if(indeg[v] == 0){
+                    q.offer(v) ;
+                }
             }
         }
-        if(pb.size() != num) return new int[0] ;
-        return pb.stream().mapToInt(i -> i).toArray();
+        if(ls.size() != num){
+            return new int[0] ;
+        }
+        int[] arr = new int[num] ;
+        for(int i = 0 ; i < num ; i++){
+            arr[i] = ls.get(i) ;
+        }
+        return arr ;
     }
 }

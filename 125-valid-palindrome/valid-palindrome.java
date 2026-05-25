66660ab03d6e81842b46c889d5d9b40401412fed
Sankalp Shukla <sankalpshukla7474@gmail.com>
2026-05-25 09:44:33 +0530

    Time: 4 ms (48.91%) | Memory: 44.7 MB (46.69%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -2,16 +2,15 @@ class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder() ;
         for(char ch : s.toCharArray()){
-            if(Character.isLetterOrDigit(ch)){
-                sb.append(Character.toLowerCase(ch)) ;
-            }
+            if(!Character.isLetterOrDigit(ch)) continue ;
+            sb.append(Character.toLowerCase(ch)) ;
         }
-        int i = 0 , j = sb.length() - 1 ;
-        while(i < j){
-            if(sb.charAt(i) != sb.charAt(j)){
+        int l = 0 , r = sb.length() - 1 ;
+        while(l < r){
+            if(sb.charAt(l) != sb.charAt(r)){
                 return false ;
             }
-            i++ ; j-- ;
+            l++ ; r-- ;
         }
         return true ;
     }

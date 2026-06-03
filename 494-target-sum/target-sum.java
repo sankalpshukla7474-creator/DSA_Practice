6939b47c08f0dbcdfb59ed958fa011578ba5dd7c
Sankalp Shukla <sankalpshukla7474@gmail.com>
2026-06-03 21:45:12 +0530

    Time: 6 ms (70.71%) | Memory: 45.3 MB (32.95%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,21 +1,36 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int range = 0 ;
+        int n = nums.length ;
+        int zc = 0 , sum = 0 ;
         for(int x : nums){
-            range += x ;
+            sum += x ;
+            if(x == 0) zc++ ;
         }
-        if(Math.abs(target) > range) return 0;
-        if((range + target) % 2 != 0) return 0;
-        int tar = (range + target) / 2 ;
-        return find(tar,nums,nums.length-1) ;
-    }
-    public int find(int tar , int[] nums , int i){
-        if(i < 0){
-            if(tar == 0) return 1;
-            return 0;
+        if (Math.abs(target) > sum) return 0;
+        int tar = sum + target ;
+        if(tar % 2 != 0) return 0 ;
+        tar /= 2 ;
+        int[][] dp = new int[n+1][tar+1] ;
+        for(int i = 0 ; i <= tar ; i++){
+            dp[0][i] = 0 ;
         }
-        int a = find(tar-nums[i],nums,i-1) ;
-        int b = find(tar,nums,i-1) ;
-        return (a+b) ;
+        for(int i = 0 ; i <= n ; i++){
+            dp[i][0] = 1 ;
+        }
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= tar ; j++){
+                if(nums[i-1] == 0){
+                    dp[i][j] = dp[i-1][j] ;
+                    continue ;
+                }
+                if(nums[i-1] <= j){
+                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j] ;
+                }
+                else{
+                    dp[i][j] = dp[i-1][j] ;
+                }
+            }
+        }
+        return dp[n][tar] * (int)Math.pow(2,zc) ;
     }
 }

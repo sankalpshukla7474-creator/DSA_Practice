968c935078c180860fec364bd6cc1811c03f8e38
Sankalp Shukla <sankalpshukla7474@gmail.com>
2026-08-11 11:59:30 +0530

    Time: 34 ms (60.70%) | Memory: 60.5 MB (5.52%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,22 +1,25 @@
 class Solution {
+    List<List<Integer>> ls = new ArrayList<>() ;
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ls = new ArrayList<>() ;
         Arrays.sort(nums) ;
-        for(int i = 0 ; i < nums.length - 2 ; i++){
+        int n = nums.length ;
+        for(int i = 0 ; i < n - 2 ; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue ;
             int tar = -1 * nums[i] ;
-            int l = i+1 , r = nums.length - 1 ;
+            int l = i + 1 , r = n - 1 ;
             while(l < r){
-                int total = nums[l] + nums[r] ;
-                if(total == tar){
+                int sum = nums[l] + nums[r] ;
+                if(sum == tar){
                     List<Integer> pb = new ArrayList<>() ;
-                    pb.add(nums[i]) ; pb.add(nums[l]) ; pb.add(nums[r]) ;
-                    ls.add(new ArrayList<>(pb)) ;
+                    pb.add(nums[i]) ;
+                    pb.add(nums[l]) ;
+                    pb.add(nums[r]) ;
+                    ls.add(pb) ;
                     while(l < r && nums[l] == nums[l+1]) l++ ;
                     while(r > l && nums[r] == nums[r-1]) r-- ;
-                    l++ ; r-- ;
+                    l++; r--;
                 }
-                else if(total > tar){
+                else if(sum > tar){
                     r-- ;
                 }
                 else{
